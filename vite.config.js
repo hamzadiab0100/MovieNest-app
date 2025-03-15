777e4f3eeac3bf02609fed3e6f33ad509c0c197b
hamza <hamzadiab500@gmail.com>
2025-03-15 14:07:41 +0200

    add rewrites

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import compression from 'vite-plugin-compression';
 
-
 export default defineConfig({
   plugins: [react(), compression()],
+  server: {
+    historyApiFallback: true
+  }
 });
